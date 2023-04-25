12090dec90130646f0295c9476e40c262161b227
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-04-25 15:30:46 -0300

    percorrendo uma lista

--- paginasDinamicas.js/main.js
+++ paginasDinamicas.js/main.js
@@ -2,8 +2,15 @@ function tocaSomPom() {
   document.querySelector("#som_tecla_pom").play();
 }
 
-
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-listaDeTeclas[0].onclick = tocaSomPom
+let contador = 0;
 
+// enquanto
+while (contador < listaDeTeclas.length) {
+  listaDeTeclas[contador ].onclick = tocaSomPom;
+
+  contador = contador + 1;
+
+  console.log(contador);
+}
